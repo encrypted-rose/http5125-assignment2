fd5e434302753ea0c419a1abd7744fc3246c3a88
Rosalia Sousa <rosev.sousac@gmail.com>
2024-02-26 15:16:51 -0400

    Add project files.

---
+++ assignment2/Controllers/j1controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assignment2.Controllers
+{
+    public class j1controller : ApiController
+    {
+
+        /// <summary>
+        /// Receives number of treats Barley the dog is receiving in numbers, which are measured in a scoring system (1 * small + 2 * medium + 3 * large)
+        /// Returns if Barley the dog is happy or sad with the amount of treats.
+        /// </summary>
+        /// <param name="small">the amount of small treats given to Barley</param>
+        /// <param name="medium">the amount of medium treats given to Barley</param>
+        /// <param name="large">the amount of large treats ggiven to Barley</param>
+        /// <returns> The status of the dog, either happy or sad, after calculating the score of the amount of treats they were given. </returns>
+        /// <example>Get api/j1controller/dogtreat/1/2/4 -> Happy!</example>
+        /// <example>Get api/j1controller/dogtreat/0/1/2 -> Sad :( </example>
+
+        [HttpGet]
+        [Route("api/j1controller/DogTreat/{small}/{medium}/{large}")]
+
+        public string DogTreat(int small, int medium, int large)
+        {
+            int var1 = 1 * small;
+            int var2 = 2 * medium;
+            int var3 = 3 * large;
+
+            if (var1 + var2 + var3 >= 10) {
+                return "Happy!";
+            }
+
+            else
+            {
+                return "Sad :(";
+            }
+
+   
+        }
+    }
+}
---
+++ assignment2/Controllers/j2controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assignment2.Controllers
+{
+    public class j2controller : ApiController
+    {/// <summary>
+     /// Verifies if input is a word of 30 letters, it's upper case and it contains at least one of the following letters: I, O, S, H, Z, X, and N. Returns a YES or NO response depending on the word inputed.
+     /// </summary>
+     /// <param name="word"></param>
+     /// <returns>If the word can be used or not for a sign that requires the letters above.</returns>
+     /// 
+        [HttpGet]
+        [Route("api/j2controller/sign/{word}")]
+
+        public string sign(string word)
+        {
+            if (word.Length > 30) && (word.Contains(char.IsUpper)
+            {
+                return "NO";
+            }
+
+            else (word.Contains("I") || (word.Contains("O")) || (word.Contains("S")) || (word.Contains("H")) || (word.Contains("Z")) || (word.Contains("X")) || (word.Contains("N"))
+            {
+                return "YES";
+            }
+        }
+    }
+}
+
+
+//I'm not completely sure about how to make this happen, I understand I need an IF statement but I'm not sure how to implement the check for upper case and for EACH letter.
